132032548d6c8d129afba352ae717807cad00c03
gyungjae-ham <gyeongjae.h.dev@gmail.com>
2026-01-20 16:33:54 +0900

    docs: telescope.lua에서 leader + f 중복되는 부분 제거

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -7,7 +7,6 @@ return {
             { '<leader>c', group = '[C]ode' },
             { '<leader>d', group = '[D]ebug' },
             { '<leader>e', group = '[E]xplorer' },
-            { '<leader>f', group = '[F]ind' },
             { '<leader>g', group = '[G]it' },
             { '<leader>J', group = '[J]ava' },
             { '<leader>t', group = '[T]ab' },
